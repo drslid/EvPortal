2272e16bab502b4984c65dc4613900088bc2291d
drslid <carbonnier.anthony@gmail.com>
2024-09-22 00:58:06 +0200

    add dpaste import from url

--- js/script.js
+++ js/script.js
@@ -17,35 +17,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const defaultPages = {
         'cinema': [
             { name: 'Prime Video', url: 'https://www.primevideo.com', order: 1 },
-            { name: 'Molotov TV', url: 'https://www.molotov.tv', order: 2 },
-            { name: 'Disney+', url: 'https://www.disneyplus.com', order: 3 },
-            { name: 'Hulu', url: 'https://www.hulu.com', order: 4 },
-            { name: 'HBO Max', url: 'https://www.hbomax.com', order: 5 },
-            { name: 'Netflix', url: 'https://www.netflix.com/fr', order: 6 },
-            { name: 'YouTube', url: 'https://www.youtube.com', order: 7 },
-            { name: 'Plex', url: 'https://app.plex.tv', order: 8 },
-            { name: 'Paramount+', url: 'https://www.paramountplus.com/fr/', order: 9 },
-            { name: 'Pluto TV', url: 'https://pluto.tv/fr/', order: 10 },
-            { name: 'Crave', url: 'https://www.crave.ca/fr', order: 11 },
-            { name: 'Starz', url: 'https://www.starz.com', order: 12 },
-            { name: 'Tubi.tv', url: 'https://www.tubi.tv', order: 13 },
-            { name: 'Xumo Play', url: 'https://www.xumo.tv', order: 14 },
-            { name: 'Crackle', url: 'https://www.crackle.com', order: 15 },
-            { name: 'Canal+', url: 'https://www.canalplus.com/', order: 16 },
-            { name: 'Apple TV+', url: 'https://tv.apple.com/fr', order: 17 },
-            { name: 'Gulli', url: 'https://replay.gulli.fr/Direct', order: 18 },
-            { name: 'TNT en Direct', url: 'https://www.tntendirect.com', order: 19 },
-            { name: 'Euronews', url: 'https://fr.euronews.com/live', order: 20 }
+            { name: 'Disney', url: 'https://www.disneyplus.com', order: 2 },
+            { name: 'Hulu', url: 'https://www.hulu.com', order: 3 },
+            { name: 'HBOMax', url: 'https://www.hbomax.com', order: 4 },
+            { name: 'Plex', url: 'https://app.plex.tv', order: 5 },
+            { name: 'Paramount', url: 'https://www.paramountplus.com/fr/', order: 6 },
+            { name: 'PlutoTV', url: 'https://pluto.tv/fr/', order: 7 },
+            { name: 'Crave', url: 'https://www.crave.ca/fr', order: 8 },
+            { name: 'Starz', url: 'https://www.starz.com', order: 9 },
+            { name: 'Canal', url: 'https://www.canalplus.com/', order: 10 },
+            { name: 'AppleTV', url: 'https://tv.apple.com/fr', order: 11 },
+            { name: 'CinePrime', url: 'https://www.youtube.com/@CINE_PRIME', order: 12 },
+            { name: 'CineMovies', url: 'https://www.youtube.com/channel/UCwIdNiqZoMq8rS4EFMuMPFQ', order: 13 },
+            { name: 'CineNanar', url: 'https://www.youtube.com/channel/UCQck1eAT_665Vn31P7qSzXQ', order: 14 },
+            { name: 'Gulli', url: 'https://replay.gulli.fr/Direct', order: 15 },
+            { name: 'AnimeSama', url: 'https://anime-sama.fr/', order: 16 }
         ],
-        'musique': [
+        'music': [
             { name: 'Apple Music', url: 'https://music.apple.com', order: 1 },
             { name: 'Deezer', url: 'https://www.deezer.com/en/channels/explore/', order: 2 },
             { name: 'SoundCloud', url: 'https://www.soundcloud.com', order: 3 },
             { name: 'Tidal', url: 'https://www.tidal.com', order: 4 },
             { name: 'Spotify', url: 'https://open.spotify.com/', order: 5 },
-            { name: 'Amazon Music', url: 'https://music.amazon.fr/', order: 6 },
-            { name: 'YouTube Music', url: 'https://music.youtube.com', order: 7 },
-            { name: 'Qobuz', url: 'https://www.qobuz.com', order: 8 },
+            { name: 'Qobuz', url: 'https://www.qobuz.com', order: 6 },
+            { name: 'ZeffyrMusic', url: 'https://www.zeffyrmusic.com/', order: 7 },
+            { name: 'AllForMusic', url: 'https://www.allformusic.fr/', order: 8 },
             { name: 'AccuRadio', url: 'https://www.accuradio.com', order: 9 },
             { name: 'iHeartRadio', url: 'https://www.iheart.com', order: 10 },
             { name: 'MixCloud', url: 'https://www.mixcloud.com', order: 11 },
@@ -55,48 +51,87 @@ document.addEventListener('DOMContentLoaded', () => {
             { name: 'TuneIn', url: 'https://tunein.com', order: 15 }
         ],
         'jeux': [
-            { name: 'Steam', url: 'https://store.steampowered.com', order: 1 },
-            { name: 'Epic Games Store', url: 'https://www.epicgames.com/store', order: 2 },
-            { name: '2048', url: 'https://jeu2048.fr', order: 3 },
-            { name: 'Solitaire', url: 'https://solitaire.com', order: 4 },
-            { name: 'Tetris', url: 'https://tetris.com', order: 5 },
-            { name: 'Minesweeper', url: 'https://minesweeperonline.com', order: 6 },
-            { name: 'Snake', url: 'https://playsnake.org', order: 7 },
-            { name: 'Chess', url: 'https://www.chess.com', order: 8 },
-            { name: 'Checkers', url: 'https://www.checkers.com', order: 9 }
+            { name: '2048', url: 'https://jeu2048.fr', order: 1 },
+            { name: 'Solitaire', url: 'https://solitaire.com', order: 2 },
+            { name: 'Tetris', url: 'https://tetris.com', order: 3 },
+            { name: 'Minesweeper', url: 'https://minesweeperonline.com', order: 4 },
+            { name: 'Snake', url: 'https://playsnake.org', order: 5 },
+            { name: 'Chess', url: 'https://www.chess.com', order: 6 },
+            { name: 'Checkers', url: 'https://www.checkers.com', order: 7 },
+            { name: 'Crazy Games', url: 'https://www.crazygames.fr/', order: 8 },
+            { name: 'Jeux.fr', url: 'https://www.jeux.fr/', order: 9 },
+            { name: '1001 Jeux', url: 'https://www.1001jeux.fr/', order: 10 },
+            { name: 'Poki', url: 'https://poki.com/fr', order: 11 },
+            { name: 'Jeux-Gratuits.com', url: 'https://www.jeux-gratuits.com/', order: 12 },
+            { name: 'PlayHop', url: 'https://playhop.com/fr', order: 13 }
+        ],
+        'meteo': [
+            { name: 'Windy', url: 'https://www.windy.com/', order: 1 },
+            { name: 'Weather.com', url: 'https://weather.com', order: 2 },
+            { name: 'LaChaineMeteo', url: 'https://www.lachainemeteo.com', order: 3 },
+            { name: 'Météo France', url: 'https://meteofrance.com', order: 4 }
         ],
         'navigation': [
-            { name: 'Google Maps', url: 'https://maps.google.com', order: 1 },
+            { name: 'GoogleMaps', url: 'https://maps.google.com', order: 1 },
             { name: 'Waze', url: 'https://www.waze.com', order: 2 },
             { name: 'Apple Maps', url: 'https://maps.apple.com', order: 3 },
             { name: 'Here WeGo', url: 'https://wego.here.com', order: 4 },
             { name: 'OpenStreetMap', url: 'https://www.openstreetmap.org', order: 5 },
             { name: 'MapQuest', url: 'https://www.mapquest.com', order: 6 },
-            { name: 'Bing Maps', url: 'https://www.bing.com/maps', order: 7 },
+            { name: 'BingMaps', url: 'https://www.bing.com/maps', order: 7 },
             { name: 'A Better Route Planner', url: 'https://abetterrouteplanner.com', order: 8 },
             { name: 'Infotraffic', url: 'https://www.infotrafic.com/', order: 9 }
         ],
-        'électrique🚗🔋': [
-            { name: 'A Better Route Planner', url: 'https://abetterrouteplanner.com', order: 1 },
+        'recharge': [
+            { name: 'ABetterRoutePlanner', url: 'https://abetterrouteplanner.com', order: 1 },
             { name: 'PlugShare', url: 'https://www.plugshare.com', order: 2 },
             { name: 'ChargePoint', url: 'https://www.chargepoint.com', order: 3 },
-            { name: 'Tesla Supercharger', url: 'https://www.tesla.com/supercharger', order: 4 },
+            { name: 'Tesla Supercharger', url: 'https://www.tesla.com/trips', order: 4 },
             { name: 'Electrify America', url: 'https://www.electrifyamerica.com', order: 5 },
             { name: 'EVgo', url: 'https://www.evgo.com', order: 6 },
-            { name: 'Zap-Map', url: 'https://www.zap-map.com', order: 7 },
+            { name: 'ZapMap', url: 'https://www.zap-map.com', order: 7 },
             { name: 'Ionity', url: 'https://www.ionity.eu', order: 8 },
             { name: 'ChargeMap', url: 'https://fr.chargemap.com/map', order: 9 },
-            { name: 'ChargePrice', url: 'https://fr.chargeprice.app/', order: 10 },
-            { name: 'Supercharge.info', url: 'https://www.supercharge.info', order: 11 },
-            { name: 'Tesla Motors Club', url: 'https://www.teslamotorsclub.com', order: 12 },
-            { name: 'Teslarati', url: 'https://www.teslarati.com', order: 13 },
-            { name: 'Teslafi', url: 'https://www.teslafi.com/', order: 14 },
-            { name: 'Windy', url: 'https://www.windy.com/', order: 15 },
-            { name: 'Weather.com', url: 'https://weather.com', order: 16 },
-            { name: 'EV Database', url: 'https://ev-database.org', order: 17 },
-            { name: 'MyEV', url: 'https://www.myev.com', order: 18 },
-            { name: 'EVCompare.io', url: 'https://evcompare.io', order: 19 },
-            { name: 'InsideEVs', url: 'https://insideevs.com', order: 20 }
+            { name: 'ChargePrice', url: 'https://fr.chargeprice.app/', order: 10 }
+        ],
+        'news': [
+            { name: 'Euronews', url: 'https://fr.euronews.com/live', order: 1 },
+            { name: 'AFP', url: 'https://www.afp.com', order: 2 },
+            { name: 'BFMTV', url: 'https://www.bfmtv.com', order: 3 },
+            { name: '20Minutes', url: 'https://www.20minutes.fr', order: 4 },
+            { name: 'LeMonde', url: 'https://www.lemonde.fr', order: 5 },
+            { name: 'LeFigaro', url: 'https://www.lefigaro.fr', order: 6 },
+            { name: 'France 24', url: 'https://www.france24.com', order: 7 },
+            { name: 'Reuters', url: 'https://www.reuters.com', order: 8 },
+            { name: 'BBC News', url: 'https://www.bbc.com/news', order: 9 },
+            { name: 'TheGuardian', url: 'https://www.theguardian.com', order: 10 },
+            { name: 'Libération', url: 'https://www.liberation.fr', order: 11 },
+            { name: 'Express', url: 'https://www.lexpress.fr', order: 12 },
+            { name: 'FranceInfo', url: 'https://www.francetvinfo.fr', order: 13 },
+            { name: 'NewYorkTimes', url: 'https://www.nytimes.com', order: 14 },
+            { name: 'TheWashingtonPost', url: 'https://www.washingtonpost.com', order: 15 },
+            { name: 'CNN', url: 'https://edition.cnn.com', order: 16 },
+            { name: 'AlJazeera', url: 'https://www.aljazeera.com', order: 17 },
+            { name: 'Mediapart', url: 'https://www.mediapart.fr', order: 18 },
+            { name: 'RTBFInfo', url: 'https://www.rtbf.be/info', order: 19 },
+            { name: 'RTNews', url: 'https://www.rt.com', order: 20 }
+        ],
+        'tv': [
+            { name: 'TNTenDirect', url: 'https://www.tntendirect.com', order: 1 },
+            { name: 'MolotovTV', url: 'https://www.molotov.tv', order: 2 },
+            { name: 'Francetv', url: 'https://www.france.tv', order: 3 },
+            { name: 'MyTF1', url: 'https://www.tf1.fr', order: 4 },
+            { name: '6play', url: 'https://www.6play.fr', order: 5 },
+            { name: 'Arte.tv', url: 'https://www.arte.tv/fr/', order: 6 },
+            { name: 'BFMTV', url: 'https://www.bfmtv.com/en-direct', order: 7 },
+            { name: 'CNEWS', url: 'https://www.cnews.fr/direct', order: 8 },
+            { name: 'France24', url: 'https://www.france24.com/fr/direct', order: 9 },
+            { name: 'LCP', url: 'https://lcp.fr/direct', order: 10 },
+            { name: 'TV5Monde', url: 'https://www.tv5monde.com', order: 11 },
+            { name: 'RTBFAuvio', url: 'https://www.rtbf.be/auvio/direct', order: 12 },
+            { name: 'Zattoo', url: 'https://zattoo.com', order: 13 },
+            { name: 'PlutoTV', url: 'https://pluto.tv/live-tv', order: 14 },
+            { name: 'StreemaTV', url: 'https://streema.com/tv', order: 15 }
         ],
         'social': [
             { name: 'Facebook', url: 'https://www.facebook.com', order: 1 },
@@ -106,7 +141,11 @@ document.addEventListener('DOMContentLoaded', () => {
             { name: 'Snapchat', url: 'https://www.snapchat.com', order: 5 },
             { name: 'TikTok', url: 'https://www.tiktok.com', order: 6 },
             { name: 'Reddit', url: 'https://www.reddit.com', order: 7 },
-            { name: 'WhatsApp Web', url: 'https://web.whatsapp.com', order: 8 }
+            { name: 'WhatsAppWeb', url: 'https://web.whatsapp.com', order: 8 },
+            { name: 'Pinterest', url: 'https://www.pinterest.com', order: 9 },
+            { name: 'Tumblr', url: 'https://www.tumblr.com', order: 10 },
+            { name: 'Discord', url: 'https://discord.com/channels/@me', order: 11 },
+            { name: 'TelegramWeb', url: 'https://web.telegram.org', order: 12 }
         ]
     };
 
@@ -362,7 +401,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function addNewPage() {
         let newPageName = newPageInput.value.trim();
         // Replace all spaces (including between words) with hyphens
-        newPageName = newPageName.replace(/\s+/g, '-');
+        newPageName = newPageName.replace(/\s+/g, '_');
         
         if (newPageName && !document.getElementById(newPageName)) {
             createPageSection(newPageName);
@@ -379,7 +418,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#addShortcutForm button').addEventListener('click', addShortcutToCurrentPage);
 
     function addShortcutToCurrentPage() {
-        const shortcutName = shortcutNameInput.value.trim();
+        const shortcutName = shortcutNameInput.value.trim().replace(/[^a-zA-Z0-9]/g, '_');
         const shortcutURL = shortcutURLInput.value.trim();
         if (shortcutName && shortcutURL) {
             addNewShortcut(currentSection, shortcutName, shortcutURL);
@@ -403,7 +442,6 @@ document.addEventListener('DOMContentLoaded', () => {
                     return;
                 }
                 const size = sizes[index];
-                // Première tentative avec 'https://'
                 const faviconUrl = `https://www.google.com/s2/favicons?sz=${size}&domain=https://${domain}`;
                 const img = new Image();
                 img.onload = function() {
@@ -414,7 +452,6 @@ document.addEventListener('DOMContentLoaded', () => {
             tryNext();
         });
     }
-
 
 
     // Ajouter un nouveau raccourci avec le meilleur favicon disponible
@@ -609,7 +646,57 @@ document.addEventListener('DOMContentLoaded', () => {
         shortcuts.forEach((shortcut, i) => shortcut.order = i + 1);  // Assigner un nouvel ordre
         localStorage.setItem(page, JSON.stringify(shortcuts));
     }
-    
-    
+
+    // Fonction pour extraire les paramètres de l'URL
+    function getQueryParam(param) {
+        const urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get(param);
+    }
+
+    // Fonction pour importer la configuration depuis Dpaste avec le code dans l'URL
+    async function importFromDpasteWithCode() {
+        const dpasteCode = getQueryParam('code'); // Récupère le code à partir de l'URL
+
+        if (dpasteCode) {
+            const dpasteURL = `https://dpaste.com/${dpasteCode}.txt`; // Construire l'URL à partir du code
+
+            try {
+                const response = await fetch(dpasteURL); // Faire la requête vers dpaste
+                if (response.ok) {
+                    const data = await response.json(); // Récupérer les données JSON
+
+                    if (typeof data === 'object') {
+                        // Vérifier que le format des données est correct
+                        if (Object.keys(data).every(page => Array.isArray(data[page]))) {
+                            // Écraser le localStorage avec les nouvelles données
+                            localStorage.setItem('pages', JSON.stringify(Object.keys(data)));
+                            Object.keys(data).forEach(page => {
+                                localStorage.setItem(page, JSON.stringify(data[page]));
+                            });
+
+                            alert('Configuration importée avec succès !');
+                            loadFromLocalStorage(); // Recharger l'interface avec les nouvelles données
+                        } else {
+                            alert('Le format des données importées est incorrect.');
+                        }
+                    } else {
+                        alert('Le format des données importées est incorrect.');
+                    }
+                } else {
+                    alert('Erreur lors de la récupération des données depuis dpaste.');
+                }
+            } catch (error) {
+                console.error('Erreur lors de la requête dpaste:', error);
+                alert('Erreur lors de la connexion à dpaste.');
+            }
+        }
+    }
+
+    // Appeler la fonction pour importer depuis dpaste si le paramètre "code" est présent
+    document.addEventListener('DOMContentLoaded', () => {
+        importFromDpasteWithCode(); // Tenter l'importation dès que la page est chargée
+    });
+
+
     loadFromLocalStorage();
 });
